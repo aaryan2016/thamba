57d8f6d49f6ff0668a2eff0066024cf7c2be23e0
Rahul Vaiwala <aaryan2016@yahoo.in>
2025-06-25 16:32:56 +0000

    feat : Herosection

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,23 @@
+export default function HeroSection() {
+  return (
+    <>
+      <div className="bg-[#facf41] text-black h-screen flex flex-col p-10 pt-20">
+        <div className="container mx-auto">
+          <div className="text-5xl md:text-8xl">ORDINARY IS TAKEN</div>
+          <div className="text-5xl md:text-8xl">WANT SOMETHING ELSE?</div>
+          <div className="pt-5 flex justify-end">
+            <div className="text-2xl">THAMBA. CREATIVE AGENCY</div>
+          </div>
+          <div className="pt-5 w-full md:w-2/3 font-light">
+            <div className="text-3xl md:text-5xl">WHAT WE DO</div>
+            <div className="text-xl md:text-3xl pt-5">
+              We are a creative design studio where we build brands that look
+              sharp, feel right, and make noise. From strategy to design, we
+              craft everything you need to stand out.
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
