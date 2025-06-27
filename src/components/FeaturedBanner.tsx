b96bf6c9695ccc449c2e72239f9503ab5d6a7c69
Rahul Vaiwala <aaryan2016@yahoo.in>
2025-06-27 16:58:36 +0000

    fix : featured banner for mobile

--- src/components/FeaturedBanner.tsx
+++ src/components/FeaturedBanner.tsx
@@ -6,19 +6,25 @@ export default function FeaturedBanner() {
       {/* Attach the ref to the main container to observe its visibility */}
       {/* The main div itself doesn't need to be a motion.div if its children are */}
       <div className="h-fit border-2 border-black p-8 text-black">
-        <div className="container mx-auto flex flex-row items-center justify-between gap-4 md:gap-8">
-          <ScrollFadeInServer className="w-1/2" yOffset={50} delay={0}>
-            <div className="text-4xl font-light md:text-6xl">FEATURED WORK</div>
+        <div className="container mx-auto flex flex-col items-start justify-between gap-4 md:flex-row md:items-center md:gap-8">
+          <ScrollFadeInServer
+            className="w-full md:w-1/2"
+            yOffset={50}
+            delay={0}
+          >
+            <div className="text-4xl font-light text-nowrap md:text-6xl">
+              FEATURED WORK
+            </div>
           </ScrollFadeInServer>
           <ScrollFadeInServer
-            className="w-1/2 flex flex-col"
+            className="hidden w-full flex-col md:flex md:w-1/2"
             yOffset={50}
             delay={0.2}
           >
-            <div className="text-sm font-light md:text-2xl">
+            <div className="font-light md:text-2xl">
               SELECTED PROJECTS THAT UNITE RESEARCH,
             </div>
-            <div className="text-sm font-light md:text-2xl">
+            <div className="font-light md:text-2xl">
               STRATEGY & CREATIVE FIREPOWER.
             </div>
           </ScrollFadeInServer>
