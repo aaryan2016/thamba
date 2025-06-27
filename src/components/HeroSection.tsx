05df06f2cec97ba8cf9cb36d27e3ccc6a2ae0536
Rahul Vaiwala <aaryan2016@yahoo.in>
2025-06-27 13:49:19 +0000

    fix : hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,12 +1,14 @@
 export default function HeroSection() {
   return (
     <>
-      <div className="bg-[#facf41] text-black h-screen flex flex-col p-2 md:p-4 pt-20">
+      <div className="bg-[#facf41] text-black h-screen flex flex-col p-4 pt-20">
         <div className="container mx-auto">
-          <div className="text-6xl md:text-8xl">ORDINARY IS TAKEN.</div>
-          <div className="text-6xl md:text-8xl">WANT SOMETHING ELSE?</div>
-          <div className="pt-5">
-            <div className="text-2xl">THAMBA. CREATIVE AGENCY</div>
+          <div className="w-fit ">
+            <div className="text-6xl md:text-8xl">ORDINARY IS TAKEN.</div>
+            <div className="text-6xl md:text-8xl">WANT SOMETHING ELSE?</div>
+            <div className="text-2xl text-right pt-2">
+              THAMBA. CREATIVE AGENCY
+            </div>
           </div>
           <div className="pt-5 w-full md:w-2/3 font-light">
             <div className="text-2xl md:text-5xl">WHAT WE DO</div>
