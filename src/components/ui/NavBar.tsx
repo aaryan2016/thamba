ce20c84f0572129eefecf49d59e08bbab4da8980
Rahul Vaiwala <aaryan2016@yahoo.in>
2025-07-06 05:12:07 +0000

    add favicon

--- src/components/ui/NavBar.tsx
+++ src/components/ui/NavBar.tsx
@@ -9,6 +9,7 @@ const navLinks = [
 	{ href: "/projects", label: "Projects" },
 	{ href: "/services", label: "Services" },
 	{ href: "/about", label: "About" },
+	{ href: "/jon", label: "Jobs" },
 	{ href: "/contact", label: "Contact" },
 ];
 
