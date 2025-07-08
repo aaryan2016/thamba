60627652d081b4a960cf9725000588b28c9a60ba
Rahul Vaiwala <aaryan2016@yahoo.in>
2025-07-08 18:33:07 +0000

    fix layout

--- src/components/PolicyCard.tsx
+++ src/components/PolicyCard.tsx
@@ -8,7 +8,7 @@ export default function PolicyCard({
 	desc: string;
 }) {
 	return (
-		<div className="flex flex-col md:flex-row items-center justify-center p-20 bg-[#fffdf5] text-black border-1 border-black gap-5 ">
+		<div className="flex flex-col md:flex-row items-center justify-center p-10 md:p-20 bg-[#fffdf5] text-black border-1 border-black gap-5 ">
 			<div className="uppercase text-2xl md:text4xl w-full md:w-1/2">
 				<ScrollFadeInServer yOffset={20} delay={0}>
 					{title}
